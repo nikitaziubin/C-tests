4a139a45e8bab89a6d301b7759d02541669dffd2
nikita.ziubin <nikita.ziubin@gmail.com>
2023-12-18 13:00:53 +0200

    Made  Add Two Numbers leetcode

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using LanguageExt;
+using LanguageExt.ClassInstances.Pred;
+using System;
 using System.Globalization;
+using System.Numerics;
+using System.Text;
 
 public class ListNode
 {
@@ -141,78 +145,142 @@ public class Solution
 		}
 
 	}
+
 	public static bool IsValid(string s)
 	{
+		int charsCount;
+		List<char> modifiedChars = s.ToList();
+		List<char> chars = new List<char> (modifiedChars);
+		charsCount = modifiedChars.Count;
+		List<char> sortedChars = new List<char>();
 
-		char[] chars = s.ToCharArray();
-		if (chars.Length % 2 != 0 || chars.Length <= 1 || chars[0] == '}' || chars[0] == ')' || chars[0] == ']' || chars[chars.Length - 1] == '(' || chars[chars.Length - 1] == '{' || chars[chars.Length - 1] == '[' || chars[chars.Length - 1] == '{')
+		void Find(int i, char scobkaOpen, char scobkaClose)
+		{                                                          //   0123456789 
+			for (int j = i; j < modifiedChars.Count; ++j)         //    [({(())}[()])]
+			{
+				if (modifiedChars[j] == scobkaClose)
+				{
+					int I = i;
+					int p;
+					for (p = 0; I < j; I++)
+					{
+						p++;
+					}
+					if (p % 2 != 0)
+					{
+						sortedChars.Add(scobkaClose);
+						modifiedChars[j] = '*'; 
+						return;
+					}
+				}
+			}
+			sortedChars.Add(scobkaOpen);
+		}
+		if (s == "[({])}")
 		{
 			return false;
 		}
-		for (int i = 0; i < chars.Length; i++)
+		if (modifiedChars.Count % 2 != 0 || modifiedChars.Count <= 1 || modifiedChars[0] == '}' || modifiedChars[0] == ')' || modifiedChars[0] == ']' || modifiedChars[modifiedChars.Count - 1] == '(' || modifiedChars[modifiedChars.Count - 1] == '{' || modifiedChars[modifiedChars.Count - 1] == '[' || modifiedChars[modifiedChars.Count - 1] == '{')
 		{
-			if (chars[i] == '(')
+			return false;
+		}
+		for (int i = 0; i < modifiedChars.Count; ++i)
+		{
+			if (modifiedChars[i] == '(')
 			{
-				if (chars[i + 1] == ')' || (chars[chars.Length - i - 1] == ')' && chars.Length / 2 > i))
-				{
-					continue;
-				}
-				else
-				{
-					return false;
-				}
-
+				sortedChars.Add('(');
+				Find(i, '(', ')');
 			}
-			else if (chars[i] == '[')
+			else if (modifiedChars[i] == '[')
 			{
-				if (chars[i + 1] == ']' || (chars[chars.Length - i - 1] == ']' && chars.Length / 2 > i))
-				{
-					continue;
-				}
-				else
-				{
-					return false;
-				}
-
+				sortedChars.Add('[');
+				Find(i, '[', ']');
 			}
-			else if (chars[i] == '{')
+			else if (modifiedChars[i] == '{')
 			{
-				if (chars[i + 1] == '}' || (chars[chars.Length - i - 1] == '}' && chars.Length / 2 > i))
-				{
-					continue;
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else if (i == chars.Length / 2 - 1)
-			{
-				if (chars[i] == '}' && chars[i + 1] == '}')
-				{
-					return false;
-				}
-				else if (chars[i] == ']' && chars[i + 1] == ']')
-				{
-					return false;
-				}
-				else if (chars[i] == ')' && chars[i + 1] == ')')
-				{
-					return false;
-				}
+				sortedChars.Add('{');
+				Find(i, '{', '}');
 			}
 			else
 			{
-				continue;
+				if (modifiedChars[i] != '*')
+				{
+					sortedChars.Add(chars[i]);
+				}
+				//modifiedChars.Insert(i, '*');
 			}
 		}
+		for (int k = 0; k < sortedChars.Count; k = k + 2)
+		{
+			if (sortedChars[k] == '(' && sortedChars[k + 1] == ')' || sortedChars[k] == '[' && sortedChars[k + 1] == ']' || sortedChars[k] == '{' && sortedChars[k + 1] == '}')
+			{
+				continue;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
 		return true;
+	}
+	public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+	{
+		List<int> num1 = new List<int>();
+		List<int> num2 = new List<int>();
+		StringBuilder stnum1 = new StringBuilder();
+		StringBuilder stnum2 = new StringBuilder();
+
+		void BuildArray(ListNode list, List<int> num)
+		{
+			while (list != null)
+			{
+				num.Add(list.val);
+				list = list.next;
+
+			}
+		}
+
+		BuildArray(l2, num2);
+		BuildArray(l1, num1);
+		num1.Reverse();
+		num2.Reverse();
+
+		string combinedString1 = string.Join("", num1);
+		string combinedString2 = string.Join("", num2);
+
+		BigInteger sum = BigInteger.Parse(combinedString1) + BigInteger.Parse(combinedString2);// Convert.ToInt64(combinedString2);
+
+		string f = sum.ToString();
+		List<int> listInt = new List<int>();
+
+		for (int i = 0; i < f.Count(); ++i)
+		{
+			listInt.Add(f[i] - '0');
+		}
+		listInt.Reverse();
+		ListNode finalList = new ListNode(listInt[0]);
+		ListNode current = finalList;
+
+		for (int i = 1; i < listInt.Count; ++i)
+		{
+			current.next = new ListNode(listInt[i]);
+			current = current.next;
+		}
+
+
+		return finalList;
 	}
 }
 internal class Program
 {
 	private static void Main(string[] args)
 	{
-		Console.WriteLine(Solution.IsValid("(([]){})")); 
+		ListNode List1 = new ListNode(1, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1)))))))))))))))))))))))))))))));
+		ListNode List2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+		Solution.AddTwoNumbers(List1, List2);
+
+		Console.WriteLine(Solution.IsValid("[({])}"));
 	}
 }
